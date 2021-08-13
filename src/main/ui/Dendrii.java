44d86b6f8078026eac5d12d5512107e1db5d416c
Kevin Ly <kevinly@alumni.ubc.ca>
2021-08-13 09:24:27 -0700

    Initial commit

---
+++ src/main/ui/Dendrii.java
@@ -0,0 +1,10 @@
+package ui;
+
+import com.formdev.flatlaf.intellijthemes.FlatArcOrangeIJTheme;
+
+public class Dendrii {
+
+    public static void main(String[] args) {
+        FlatArcOrangeIJTheme.install();
+    }
+}
